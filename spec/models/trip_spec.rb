4b8607cf00f839628a134fef656e07eecb0e45e2
Jude Lawson <lawsonjdutton@gmail.com>
2018-05-25 15:27:25 -0600

    TESTS Trip model validations and relationships

---
+++ spec/models/trip_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Trip do
+  describe 'Validations' do
+    it { should validate_presence_of(:duration) }
+    it { should validate_presence_of(:start_date) }
+    it { should validate_presence_of(:end_date) }
+    it { should validate_presence_of(:start_station_id) }
+    it { should validate_presence_of(:end_station_id) }
+    it { should validate_presence_of(:bike_id) }
+    it { should validate_presence_of(:subscription_type) }
+    it { should validate_presence_of(:zip_code) }
+  end
+
+  desribe 'Relationships' do
+    it { should belong_to(:station) }
+  end
+end
