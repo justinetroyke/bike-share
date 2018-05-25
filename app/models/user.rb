550da3fd0dc47663c108b68aa1ec7703a03dd85b
Justine <jjtroyke@gmail.com>
2018-05-25 17:41:35 -0600

    user info validated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  validates :role, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :address, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates_presence_of :password, require: true, on: :create
+
+  has_secure_password
+
+  has_many :orders
 end
