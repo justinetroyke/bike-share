4b33f9625b810f01d7b27a80edb936f12ece99e8
Jude Lawson <lawsonjdutton@gmail.com>
2018-05-26 15:04:17 -0600

    Adds in user fixtures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,4 +10,12 @@ RSpec.configure do |config|
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
   config.filter_run_when_matching :focus
+
+  config.before :each do
+    # Create Users
+    @user1 = User.create!(role: 1, username: 'user1', password: 'user1spassword', address: '111 Not An Address', first_name: 'User', last_name: 'One')
+    @user2 = User.create!(role: 0, username: 'user2', password: 'user2spassword', address: '222 Not An Address', first_name: 'User', last_name: 'Two')
+    @user3 = User.create!(role: 0, username: 'user3', password: 'user3spassword', address: '333 Not An Address', first_name: 'User', last_name: 'Three')
+    @user4 = User.create!(role: 0, username: 'user4', password: 'user4spassword', address: '444 Not An Address', first_name: 'User', last_name: 'Four')
+  end
 end
