cca03940dceeba23372eaa464b64f0f0174c5aee
Justine <jjtroyke@gmail.com>
2018-05-27 10:22:14 -0600

    TESTS issue #12 visitor sees all trip attributes on show page

---
+++ spec/features/visitor/trips/user_visits_trip_show_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe 'When you click on a specific trip' do
+  context 'as a visitor' do
+    it 'shows all attributes of a trip' do
+      station = Station.create!(
+        name: 'Union Station',
+        dock_count: 12,
+        city: 'Denver',
+        installation_date: Time.parse('2010-05-05 00:00:00')
+      )
+      trip = Trip.create!(
+        duration: 1315,
+        start_date: Time.parse('2018-05-05 13:10:00'),
+        end_date: Time.parse('2018-05-05 14:02:00'),
+        start_station_id: station.id,
+        end_station_id: station.id,
+        bike_id: 64,
+        subscription_type: 'subscriber',
+        zip_code: 80202
+      )
+
+      visit trip_path(trip)
+
+      expect(current_page).to have_content(trip.duration)
+      expect(current_page).to have_content(trip.start_date)
+      expect(current_page).to have_content(trip.end_date)
+      expect(current_page).to have_content(trip.start_station_id)
+      expect(current_page).to have_content(trip.end_station_id)
+      expect(current_page).to have_content(trip.bike_id)
+      expect(current_page).to have_content(trip.subscription_type)
+      expect(current_page).to have_content(trip.zip_code)
+    end
+  end
+end
