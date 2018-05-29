fa6ad59e6e07bc7024073cc458e93a65988c98af
Jude Lawson <lawsonjdutton@gmail.com>
2018-05-29 13:27:44 -0600

    Adds in slug attribute to Station model, and adds in empty StationsController

--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -12,4 +12,20 @@ RSpec.describe Station do
     it { is_expected.to have_many(:start_trips) }
     it { is_expected.to have_many(:end_trips) }
   end
+
+  describe 'Insatance Methods' do
+    describe '#generate_slug' do
+      it 'should create a slug out of the station name' do
+        station1 = Station.new(name: 'Station Name',
+                               dock_count: 12,
+                               city: 'Denver',
+                               installation_date: '2015-01-02',
+                               slug: nil)
+
+        station1.generate_slug
+
+        expect(station1.slug).to eq(station1.name.parameterize)
+      end
+    end
+  end
 end
