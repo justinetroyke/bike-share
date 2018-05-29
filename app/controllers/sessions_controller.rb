14f2f1703532884a4df6bc90879a1af9ddc038b1
zdcthomas <zdcthomas@yahoo.com>
2018-05-29 13:53:07 -0600

    implements admin logs in user story ATP

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,10 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       if current_admin?
+        flash[:notice] = "Logged in as Admin User: #{current_user.username}"
         redirect_to admin_dashboard_path
       else
+        flash[:notice] = "Logged in as User: #{current_user.username}"
         redirect_to dashboard_path
       end
     else
