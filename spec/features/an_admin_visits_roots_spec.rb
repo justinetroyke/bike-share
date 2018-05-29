cb65f72ca4f6d8f7d00ca95243b12e7013c7735f
zdcthomas <zdcthomas@yahoo.com>
2018-05-29 13:05:19 -0600

    fixes typo in admin logs in spec

--- spec/features/an_admin_visits_roots_spec.rb
+++ spec/features/an_admin_visits_roots_spec.rb
@@ -4,13 +4,13 @@ RSpec.feature "An Admin Logs in", type: :feature do
   describe 'An unauthenticated admin' do
     before (:all) do 
       @password = 'password'
-      @admin = User.new(username:'username',
-                       password:password,
+      @username = 'username'
+      admin = User.create!(username: @username,
+                       password: @password,
                        last_name: 'ever',
                        first_name:'greatest',
                        address:'1234 awesome street',
                        role:1)
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(:@admin)
     end
     context 'visiting the root path' do
       it 'should show a link for login' do
@@ -19,22 +19,28 @@ RSpec.feature "An Admin Logs in", type: :feature do
       end
     end
     context 'visits the login page' do
-      visit root_path
-      click_on 'Log In'
+      it 'should show the relevant user information and not show log in' do
+        visit root_path
+        click_on 'Log In'
 
-      expect(current_path).to eq(login_path)
-      fill_in :username, with: admin.username
-      fill_in :password, with: @password
-      click_on('Log In')
+        expect(current_path).to eq(login_path)
+        expect(page).to have_field('username')
+        expect(page).to have_field('password')
+        fill_in ('username'), with: @username
+        fill_in ('password'), with: @password
+        within 'content' do
+          click_on('Log In')
+        end
 
-      expect(current_path).to eq(admin_dashboard_path)
-      expect(page).to have_content("Logged in as Admin User: #{admin.username}")
-      expect(page).to have_content(admin.first_name)
-      expect(page).to have_content(admin.username)
-      expect(page).to have_content(admin.last_name)
-      expect(page).to have_content(admin.address)
-      expect(page).to_not have_link('Log In')
-      expect(page).to have_link('Log Out')
+        expect(current_path).to eq(admin_dashboard_path)
+        expect(page).to have_content("Logged in as Admin User: #{admin.username}")
+        expect(page).to have_content(admin.first_name)
+        expect(page).to have_content(admin.username)
+        expect(page).to have_content(admin.last_name)
+        expect(page).to have_content(admin.address)
+        expect(page).to_not have_link('Log In')
+        expect(page).to have_link('Log Out')
+      end
     end
   end
 end
