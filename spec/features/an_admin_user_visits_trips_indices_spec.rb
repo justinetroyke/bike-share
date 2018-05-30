5b62513043c5f0c356805888b57374f35ae55d63
zdcthomas <zdcthomas@yahoo.com>
2018-05-29 19:51:41 -0600

    Implements admin trip deletion

--- spec/features/an_admin_user_visits_trips_indices_spec.rb
+++ spec/features/an_admin_user_visits_trips_indices_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "An Admin User Visits Trips Index", type: :feature do
   describe 'an admin user' do
     context 'visiting the trips index' do
 
-      before(:all) do
+      before(:each) do
         @station1 = Station.create!(name:'name',
           dock_count:5,
           city:'denver',
@@ -19,7 +19,7 @@ RSpec.feature "An Admin User Visits Trips Index", type: :feature do
             start_date: DateTime.now,
             bike_id: 10,
             subscription_type: 0,
-            zip_code: 23456,
+            zip_code: 94053,
             start_station_id: @station1.id,
             end_station_id: @station2.id,
             end_date: DateTime.now)
@@ -78,6 +78,24 @@ RSpec.feature "An Admin User Visits Trips Index", type: :feature do
           within('li:nth-child(1)') do
             expect(page).to have_button('Delete')
           end
+        end
+        it 'should delete the related trip' do
+          admin = User.create!(username:'username',
+            password:'password',
+            first_name:'john',
+            last_name:'smith',
+            address:'1234 address',
+            role:1)
+          allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+          visit trips_path
+
+          within('li:nth-child(1)') do
+            click_on('Delete')
+          end
+
+          expect(page).to_not have_content(@trip1.zip_code)
+          expect(page).to_not have_content(@trip1.end_date)
         end
       end
     end
