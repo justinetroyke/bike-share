47644c08f2229823abd52ecd4450935e0923083b
zdcthomas <zdcthomas@yahoo.com>
2018-05-30 17:41:00 -0600

    TESTS admin station deletion

---
+++ spec/features/admin/stations/admin_can_delete_station_from_index_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'Station index Page (Admin)' do
+  describe 'An Admin' do
+    context 'visitng the stations index page' do
+      describe 'and clicking the delete button next to the station' do
+        it 'should delete the station' do
+          admin = User.create!(username:'fmqueen',
+                               first_name:'freddy',
+                               last_name:'mercury',
+                               password:'bohemian',
+                               role:1)
+          station = Station.create(name:'downtown',
+                         dock_count:4,
+                         city:'denver',
+                         installation_date:Time.now)
+          allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+          
+          visit stations_path
+          click_on('Delete')
+
+          expect(page).to_not have_content(station.name)
+          expect(page).to have_content("#{station.name} Deleted")
+        end
+      end
+    end
+  end
+end
