1d2f0fdc80f0d9d923317e311389216e9a904895
Jude Lawson <lawsonjdutton@gmail.com>
2018-05-30 22:15:58 -0600

    TESTS numericality of price

--- app/models/accessory.rb
+++ app/models/accessory.rb
@@ -4,6 +4,7 @@ class Accessory < ApplicationRecord
                         :description,
                         :status
   validates :status, inclusion: { in: [0,1] }
+  validates :price, numericality: { :greater_than => 0 }
 
   has_many :order_accessories
   has_many :orders, through: :order_accessories
--- spec/models/accessory_spec.rb
+++ spec/models/accessory_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Accessory do
   describe 'Validations' do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:price) }
+    it { should validate_numericality_of(:price).is_greater_than(0) }
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:status) }
     it { should validate_inclusion_of(:status).in_array([0,1]) }
