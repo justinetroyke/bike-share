713c7a299fd8a6d4201e26fcae974245e4c831b8
Jude Lawson <lawsonjdutton@gmail.com>
2018-05-30 20:54:11 -0600

    TESTS uniqueness of new accessory titles

--- app/models/accessory.rb
+++ app/models/accessory.rb
@@ -2,7 +2,8 @@ class Accessory < ApplicationRecord
   validates_presence_of :title,
                         :price,
                         :description
-
+  validates_uniqueness_of :title
+  
   has_many :order_accessories
   has_many :orders, through: :order_accessories
 end
--- spec/models/accessory_spec.rb
+++ spec/models/accessory_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Accessory do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:price) }
     it { should validate_presence_of(:description) }
+    it { should validate_uniqueness_of(:title) }
   end
 
   describe 'Relationships' do
