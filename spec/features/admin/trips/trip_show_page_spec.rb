9c4b5f59e766f4e6f1abc3ea42100047c12b15cb
Jude Lawson <lawsonjdutton@gmail.com>
2018-05-31 15:34:06 -0600

    Implements functionality for showing information on trip show page. Additional Edit and Delete functionality is WIP

--- spec/features/admin/trips/trip_show_page_spec.rb
+++ spec/features/admin/trips/trip_show_page_spec.rb
@@ -14,22 +14,22 @@ RSpec.describe 'Trip Show Page (Admin)' do
       duration: 1315,
       start_date: Time.parse('2018-05-05 13:10:00'),
       end_date: Time.parse('2018-05-05 14:02:00'),
-      start_station_id: station.id,
-      end_station_id: station.id,
+      start_station_id: @station.id,
+      end_station_id: @station.id,
       bike_id: 64,
       subscription_type: 'subscriber',
       zip_code: 80202
     )
-    allow_anh_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
-    visit trip_path(trip)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+    visit trip_path(@trip)
   end
   describe 'An admin user visits the show page for a specific trip' do
     it 'they should see all of the trips attributes' do
       expect(page).to have_content("Duration: #{@trip.duration}")
       expect(page).to have_content("Start Date: #{@trip.start_date}")
       expect(page).to have_content("End Date: #{@trip.end_date}")
-      expect(page).to have_content("Start Station: #{@trip.start_station_id}")
-      expect(page).to have_content("End Station: #{@trip.end_station_id}")
+      expect(page).to have_content("Start Station: #{@station.name}, ID: #{@trip.start_station_id}")
+      expect(page).to have_content("End Station: #{@station.name}, ID: #{@trip.end_station_id}")
       expect(page).to have_content("Bike ID: #{@trip.bike_id}")
       expect(page).to have_content("Subscription: #{@trip.subscription_type}")
       expect(page).to have_content("Zip Code: #{@trip.zip_code}")
@@ -45,14 +45,14 @@ RSpec.describe 'Trip Show Page (Admin)' do
       expect(page).to have_link('Delete')
       click_link 'Delete'
       expect(page).to have_current_path(trips_path)
-      expect(page).to_not have_content("Duration: #{@trip.duration}")
-      expect(page).to_not have_content("Start Date: #{@trip.start_date}")
-      expect(page).to_not have_content("End Date: #{@trip.end_date}")
-      expect(page).to_not have_content("Start Station: #{@trip.start_station_id}")
-      expect(page).to_not have_content("End Station: #{@trip.end_station_id}")
-      expect(page).to_not have_content("Bike ID: #{@trip.bike_id}")
-      expect(page).to_not have_content("Subscription: #{@trip.subscription_type}")
-      expect(page).to_not have_content("Zip Code: #{@trip.zip_code}")
+      expect(page).to_not have_content(@trip.duration)
+      expect(page).to_not have_content(@trip.start_date)
+      expect(page).to_not have_content(@trip.end_date)
+      expect(page).to_not have_content(@trip.start_station_id)
+      expect(page).to_not have_content(@trip.end_station_id)
+      expect(page).to_not have_content(@trip.bike_id)
+      expect(page).to_not have_content(@trip.subscription_type)
+      expect(page).to_not have_content(@trip.zip_code)
     end
   end
 end
