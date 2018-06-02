2855484c79789375207294d83d610dda46049375
zdcthomas <zdcthomas@yahoo.com>
2018-06-01 21:33:50 -0600

    implements user story 37

--- spec/features/admin/station/new_admin_stations_spec.rb
+++ spec/features/admin/station/new_admin_stations_spec.rb
@@ -14,19 +14,19 @@ RSpec.feature "NewAdminStations", type: :feature do
 
         visit new_admin_station_path
 
-        name = "station name is something"
+        station_name = "station name is something"
         dock_count = 10
         city = 'Denver'
         installation_date = Time.now
         
-        fill_in 'station[name]', with: name
+        fill_in 'station[name]', with: station_name
         fill_in 'station[dock_count]', with: dock_count
         fill_in 'station[city]', with: city
         fill_in 'station[installation_date]', with: installation_date
-        click_on('Create Station')
-
-        station = Station.find_by(name:name)
-        expect(current_path).to eq(station_path(station))
+        click_on("Create Station")
+        
+        station = Station.find_by(name:station_name)
+        expect(current_path).to eq(station_path(station.slug))
         expect(page).to have_content("#{station.name} Station Created")
       end
     end
