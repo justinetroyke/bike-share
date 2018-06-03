0a874b0aa5c058cf16764dc4053da59de34a647d
zdcthomas <zdcthomas@yahoo.com>
2018-06-02 20:55:38 -0600

    implements method calls in stations controller

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -3,6 +3,11 @@ class StationsController < ApplicationController
     @station = Station.find_by(slug:params[:slug])
     @start_trips = @station.start_trips
     @end_trips = @station.end_trips
+    @most_frequent_destination = @station.most_frequent_destination
+    @most_frequent_origination = @station.most_frequent_origination
+    @busiest_date = @station.busiest_date
+    @most_frequent_zip_code = @station.most_frequent_zip_code
+    @favorite_bike = @station.favorite_bike
   end
 
   def index
