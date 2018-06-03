b05274d4a2bebc53c7d71f03a886b879eefe934a
zdcthomas <zdcthomas@yahoo.com>
2018-06-02 19:33:43 -0600

    adds show page spec file

---
+++ spec/features/users/stations/show_page_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.feature "Station Show page", type: :feature do
+  describe 'A User' do 
+    describe 'visits the station show page' do
+      it 'should show the number of rides started at that station' do
+      end
+      it 'should show the Number of rides ended at this station' do
+      end
+      it 'should show the Most frequent destination station (for rides that began at this station)' do
+      end
+      it 'should the Most frequent origination station (for rides that ended at this station' do
+      end
+      it 'should the Date with the highest number of trips started at this station' do
+      end
+      it 'should the Most frequent zip code for users starting trips at this station' do
+      end
+      it 'should show the Bike ID most frequently starting a trip at this station.' do
+      end
+    end
+  end
+end
