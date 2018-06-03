c554fa6612881847f94499096f2c51502608b823
Jude Lawson <lawsonjdutton@gmail.com>
2018-06-03 15:09:04 -0600

    Adds in creators for Stations and Trips to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@ trips_data = CSV.read('db/csv/trips.csv', headers: true, header_converters: :sym
 
 
 stations_data.each do |row|
-  Station.create!(name: row[:name], dock_count: row[:dock_count], city: row[:city], installation_date: row[:installation_date])
+  Station.find_or_create_by!(name: row[:name], dock_count: row[:dock_count], city: row[:city], installation_date: row[:installation_date])
 end
 
 trips_data.each do |row|
-  Trip.create!(duration: row[:duration], start_date: row[:start_date], end_date: row[:end_date], subscription_type: row[:subscription_type], zip_code: row[:zip_code], start_station_id: row[:start_station_id], end_station_id: row[:end_station_id], bike_id: row[:bike_id])
+  Trip.find_or_create_by!(duration: row[:duration], start_date: row[:start_date], end_date: row[:end_date], subscription_type: row[:subscription_type], zip_code: row[:zip_code], start_station_id: row[:start_station_id], end_station_id: row[:end_station_id], bike_id: row[:bike_id])
 end
