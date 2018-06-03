3d5f66b9da274988ea253be9caf2530521811e6a
Jude Lawson <lawsonjdutton@gmail.com>
2018-06-02 19:38:56 -0600

    Implementing most recently installed station in stations dashboard index view is WIP

--- app/controllers/stations/dashboard_controller.rb
+++ app/controllers/stations/dashboard_controller.rb
@@ -1,5 +1,14 @@
 class Stations::DashboardController < ApplicationController
   def index
-    @total = Station.all.count
+    @stations = Station.all
+    @total = @stations.count
+    @average_bikes_per_station = @stations.average(:dock_count).round(2)
+    @most_bikes = @stations.maximum(:dock_count)
+    @stations_with_most_bikes = Station.where(dock_count: @most_bikes)
+    @least_bikes = @stations.minimum(:dock_count)
+    @stations_with_least_bikes = Station.where(dock_count: @least_bikes)
+
+    newest_install_date = Station.minimum(:installation_date)
+    @newest_station = Station.where(installation_date: newest_install_date)
   end
 end
--- spec/features/dashboards/stations_dashboard_spec.rb
+++ spec/features/dashboards/stations_dashboard_spec.rb
@@ -6,19 +6,19 @@ RSpec.describe 'Stations Dashboard' do
     
     # I created stations a few times to salt the calculations for testing a bit
     
-    @stations << Station.create!(name: "Station Least", dock_count: 1, city: "City 0", installation_date: DateTime.now)
-    @stations << Station.create!(name: "Station Least Two", dock_count: 1, city: "City 0", installation_date: DateTime.now)
+    @stations << Station.create!(name: "Station Least", dock_count: 1, city: "City 0", installation_date: Date.parse('2014-01-01'))
+    @stations << Station.create!(name: "Station Least Two", dock_count: 1, city: "City 0", installation_date: Date.parse('2015-01-01'))
     
     18.times do |num|
-      @stations << Station.create!(name: "Station #{num}", dock_count: 2, city: "City #{num}", installation_date: DateTime.now)
+      @stations << Station.create!(name: "Station #{num}", dock_count: 2, city: "City #{num}", installation_date: Date.parse('2015-01-01'))
     end
     
     20.times do |num|
-      @stations << Station.create!(name: "Station #{num}", dock_count: 3, city: "City #{num}", installation_date: DateTime.now)
+      @stations << Station.create!(name: "Station #{num}", dock_count: 3, city: "City #{num}", installation_date: Date.parse('2015-01-01'))
     end
     
-    @stations << Station.create!(name: "Station Most", dock_count: 4, city: "City 40", installation_date: DateTime.now)
-    @stations << Station.create!(name: "Station Most Two", dock_count: 4, city: "City 40", installation_date: DateTime.now)
+    @stations << Station.create!(name: "Station Most", dock_count: 4, city: "City 40", installation_date: Date.parse('2015-01-01'))
+    @stations << Station.create!(name: "Station Most Two", dock_count: 4, city: "City 40", installation_date: Date.parse('2016-01-01'))
     
     visit stations_dashboard_path
   end
@@ -32,19 +32,20 @@ RSpec.describe 'Stations Dashboard' do
     end
 
     it 'should show the most bikes available at a station or stations and that station(s)\' name(s)' do
-      expect(page).to have_content("Most Bikes: 4 at station(s):\nStation Most\nStation Most Two")
+      expect(page).to have_content("Most Bikes: 4 at station(s): Station Most Station Most Two")
     end
 
     it 'should show the least bikes available at a station or stations and that station(s)\' name(s)' do
-      expect(page).to have_content("Least Bikes: 1 at station(s):\nStation Least\nStation Least Two")
+      expect(page).to have_content("Least Bikes: 1 at station(s): Station Least Station Least Two")
     end
 
     it 'should show the most recently installed station' do
-      expect(page).to have_content("Most Recently Installed Station: #{@stations.last.name}")
+      save_and_open_page
+      expect(page).to have_content("Most Recently Installed Station: #{@stations.first.name}")
     end
 
     it 'should show the oldest station' do
-      expect(page).to have_content("Most Recently Installed Station: #{@stations.first.name}")
+      expect(page).to have_content("Oldest Station: #{@stations.last.name}")
     end
   end
 end
