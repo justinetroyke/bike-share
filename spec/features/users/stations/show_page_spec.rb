58ffff1b9985150130a3ab488a00eff89505cbe3
zdcthomas <zdcthomas@yahoo.com>
2018-06-02 20:20:36 -0600

    fixes typo in station show tests

--- spec/features/users/stations/show_page_spec.rb
+++ spec/features/users/stations/show_page_spec.rb
@@ -40,31 +40,34 @@ RSpec.feature "Station Show page", type: :feature do
                        subscription_type:2)
         end
       end
-      end
       it 'should show the number of rides started at that station' do
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
         visit station_page(@station)
         expect(page).to have_content('Trips started at this station: 10')
       end
+
       it 'should show the Number of rides ended at this station' do
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
         visit station_page(@station)
         expect(page).to have_content('Trips ended at this station: 7')
       end
+
       it 'should show the Most frequent destination station (for rides that began at this station)' do
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
         visit station_page(@station)
         expect(page).to have_content("Most frequent destination, from this station: #{@station2.name}")
       end
+
       it 'should the Most frequent origination station (for rides that ended at this station' do
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
         visit station_page(@station)
         expect(page).to have_content("Most frequent origination point, for this station: #{@station2.name}")
       end
+
       it 'should show the Date with the highest number of trips started at this station' do
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
