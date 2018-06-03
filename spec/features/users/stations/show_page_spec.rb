0b0bafdf88a0be2257b8da1c95199f8ed8a7a39c
zdcthomas <zdcthomas@yahoo.com>
2018-06-02 19:42:16 -0600

    adds station and user creation

--- spec/features/users/stations/show_page_spec.rb
+++ spec/features/users/stations/show_page_spec.rb
@@ -3,6 +3,19 @@ require 'rails_helper'
 RSpec.feature "Station Show page", type: :feature do
   describe 'A User' do 
     describe 'visits the station show page' do
+      before(:all) do
+        @user = User.create!(username:'someone',
+                            first_name:'whoever',
+                            last_name:'someone else',
+                            role:0,
+                            password: 'password',
+                            address:'most places eventually')
+        @station = Station.create!(name: 'Union Station', 
+                                   dock_count: 12, 
+                                   city: 'Denver', 
+                                   installation_date: DateTime.now)
+        
+      end
       it 'should show the number of rides started at that station' do
       end
       it 'should show the Number of rides ended at this station' do
