64161e6e543f9ccb53ece489ed45cddb0b64adf5
zdcthomas <zdcthomas@yahoo.com>
2018-06-02 20:25:08 -0600

    fixes station slug error in station show page tests

--- spec/features/users/stations/show_page_spec.rb
+++ spec/features/users/stations/show_page_spec.rb
@@ -43,48 +43,48 @@ RSpec.feature "Station Show page", type: :feature do
       it 'should show the number of rides started at that station' do
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
-        visit station_page(@station)
+        visit station_path(@station.slug)
         expect(page).to have_content('Trips started at this station: 10')
       end
 
       it 'should show the Number of rides ended at this station' do
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
-        visit station_page(@station)
+        visit station_path(@station.slug)
         expect(page).to have_content('Trips ended at this station: 7')
       end
 
       it 'should show the Most frequent destination station (for rides that began at this station)' do
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
-        visit station_page(@station)
+        visit station_path(@station.slug)
         expect(page).to have_content("Most frequent destination, from this station: #{@station2.name}")
       end
 
       it 'should the Most frequent origination station (for rides that ended at this station' do
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
-        visit station_page(@station)
+        visit station_path(@station.slug)
         expect(page).to have_content("Most frequent origination point, for this station: #{@station2.name}")
       end
 
       it 'should show the Date with the highest number of trips started at this station' do
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
-        visit station_page(@station)
-        expect(page).to have_content("Busiest date for #{@station.name}: #{Date.now}")
+        visit station_path(@station.slug)
+        expect(page).to have_content("Busiest date for #{@station.name}: #{Time.now}")
       end
       it 'should show the Most frequent zip code for users starting trips at this station' do
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
-        visit station_page(@station)
+        visit station_path(@station.slug)
         expect(page).to have_content("Most frequent zipcode of users using this station: #{@station.most_frequent_zip}")
       end
       it 'should show the Bike ID most frequently starting a trip at this station.' do
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
 
-        visit station_page(@station)
-        expect(page).to have_content("Most used bike at #{station.name}: #{@station.most_frequent_bike}")
+        visit station_path(@station.slug)
+        expect(page).to have_content("Most used bike at #{@station.name}: #{@station.most_frequent_bike}")
       end
     end
   end
