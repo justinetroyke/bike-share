1af5f43e4c2c5629e185378200ec1871da8404cb
Jude Lawson <lawsonjdutton@gmail.com>
2018-06-05 14:07:50 -0600

    Completes cart styling

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -15,7 +15,7 @@ class CartsController < ApplicationController
   def destroy
     @accessory = Accessory.find(params[:accesory_id])
     @cart.remove_accessory(@accessory.id.to_s)
-    flash[:notice] = "Successfully removed #{@accessory.title} from your cart."
+    flash[:info] = "Successfully removed '#{@accessory.title}' from your cart."
     flash[:link] = ["#{@accessory.title}", accessory_path(@accessory)]
     redirect_to '/cart'
   end
